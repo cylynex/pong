22baa2653d2e77fc462a2a3e5b5cdc84c3a481ad
Santo Commarato <cylynex@gmail.com>
2018-04-27 10:56:12 -0600

    Ball physics and score in.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
 	public GameObject ball;
+	public GameObject ballPrefab;
+	Vector3 ballStartLocation;
+	public int player1Score = 0;
+	public int player2Score = 0;
 
+	public Text p1ScoreBoard;
+	public Text p2ScoreBoard;
+
+	void Start() {
+		ballStartLocation = new Vector3(5,0,0);
+	}
 	void Update () {
 		if (Input.GetKeyUp(KeyCode.Space)) {
 			ball.GetComponent<Rigidbody2D>().velocity = new Vector2(5f,5f);
 		}	
+	}
+
+
+	public void UpdateScores() {
+		p1ScoreBoard.text = player1Score.ToString();
+		p2ScoreBoard.text = player2Score.ToString();
+	}
+
+
+	// Reset the ball
+	public void ResetBall() {
+		//Destroy(ball);
+		//Instantiate(ballPrefab,ballStartLocation,Quaternion.identity);
+		ball.transform.position = ballStartLocation;
+		ball.GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
 	}
 }
---
+++ Assets/Scripts/Player1Score.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player1Score : MonoBehaviour {
+
+	public GameManager scorekeeper;
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		scorekeeper.player1Score++;
+		scorekeeper.UpdateScores();
+		scorekeeper.ResetBall();
+	}
+}
